57f4ff171fc9d2b8fe07c6b028d86b243a408c5c
Jhon Philip Resma <jhonphilipresma46@gmail.com>
2026-03-14 19:26:09 +0800

    Add files via upload

---
+++ RESMA/ACTIVITY_4/Activity4.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+int main()
+{
+double num;
+int length;
+
+char text[50];
+
+printf("Enter a word : %s", text);
+scanf("%s", &text);
+text[0] = toupper(text[0]);
+
+printf("Enter a number : ");
+scanf("%lf", &num);
+
+
+length = strlen(text);
+printf("Length : %d\n", length);
+printf("Square root : %.2lf\n", sqrt(num));
+printf("The capital of the word is : %s", text);
+return 0;
+}
